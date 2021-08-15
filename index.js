cffee706945c2257df09c0fca627fd8a81b18c34
Evgeniy Khristenko <e.khristenko@ostrovok.ru>
2021-08-15 12:19:20 +0500

    asdf

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { WebClient } = require("@slack/web-api");
 
-// let isStarted = false;
+let isStarted = false;
+
+
+const handleRequest = async (request) => {
+  if (!isStarted || isStarted) {
+    isStarted = true;
 
 const client = new WebClient("xoxb-468242695412-2394252523313-F1U34u14bNYWRv5ohoONKAFP");
 
@@ -16,4 +21,19 @@ const publishMessage = (id, text) => {
     });
 }
 
-publishMessage('test-bots', 'test');
+  publishMessage('#test-bots', 'test');
+}
+
+
+  // if we found data somewhere - return it
+  return new Response('success, I guess', { status: 200 });
+};
+
+// listen and handle requests
+addEventListener("fetch", (event) => {
+event.respondWith(
+  handleRequest(event.request).catch(
+    (err) => new Response(err.stack, { status: 500 })
+  )
+);
+});
