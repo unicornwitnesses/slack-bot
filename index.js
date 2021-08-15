4d0436ebe4002dcaf45219f8e600e3606f54c7d0
Evgeniy Khristenko <e.khristenko@ostrovok.ru>
2021-08-15 13:22:36 +0500

    asdf

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get('/', (req, res) => {
   res.send('bklasjdfklj');
 });
 
-app.listen(process.env.port || 8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Example app listening on port 8000!')
 });
