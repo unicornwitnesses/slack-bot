21137d744441e01f6eb53584c3ce50693bb01a43
Evgeniy Khristenko <e.khristenko@ostrovok.ru>
2021-08-16 12:11:47 +0500

    fix

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const { WebClient } = require("@slack/web-api");
-
 const express = require('express')
 const app = express();
+
 app.get('/notify', (req, res) => {
+  const channel = req.query.channel;
+  const message = req.query.message || 'Привет! Кто чем занимается сегодня?';
   const client = new WebClient("xoxb-468242695412-2394252523313-F1U34u14bNYWRv5ohoONKAFP");
+
+  if (!channel) return;
+
   client.chat.postMessage({
     token: "xoxb-468242695412-2394252523313-F1U34u14bNYWRv5ohoONKAFP",
-    channel: '#activities',
-    text: 'Привет! Кто чем занимается сегодня?'
+    channel: `#${channel}`,
+    text: message,
   })
-  .then(result => console.log(result))
-  .then(() => res.send('Welcome to learn backend with express!'))
-  .catch(err => {
-    console.error(err, 'error');
-    res.send('bad')
-  });
+    .then(result => console.log(result))
+    .then(() => res.status(200).json({
+      message: 'Message sent successfully',
+    }))
+    .catch(err => {
+      console.error(err, 'error');
+      res.status(500).json({
+        error: err.message,
+      });
+    });
+});
+
+app.get('/notify-test', (req, res) => {
+
+});
+
+app.get('/say', (req, res) => {
+  res.send('say something');
 });
 
 app.get('/', (req, res) => {
